e6e9f0c11518a4c4662f87e24bdd2807d53dd263
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2025-06-19 19:02:15 +0300

    feat: Implemented publishing via npm

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+import { series, src, dest } from "gulp";
+import clean_ from "gulp-clean";
+
+export function clean() {
+    return src("assets", { allowEmpty: true }).pipe(clean_());
+}
+
+export function assembly() {
+    return src("src/main/resources/**/*.yaml")
+        .pipe(dest("assets"));
+}
+
+// exports.default = series(clean, assembly);
