e1303f6ce1562950aadb7dd8cecef9a63c9176c2
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2025-07-08 14:18:12 +0300

    fix: fixed schemas related to service and added tests

---
+++ src/test/java/org/qubership/integration/platform/schemas/SchemaOnSamplesTest.java
@@ -0,0 +1,92 @@
+package org.qubership.integration.platform.schemas;
+
+import com.networknt.schema.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.support.ParameterDeclarations;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SchemaOnSamplesTest {
+    private static final Logger logger = Logger.getLogger(SchemaOnSamplesTest.class.getName());
+
+    public static final Map<String, String> testMap = Map.of(
+            "http://qubership.org/schemas/product/qip/service/service.schema.yaml", "samples/service/*.yaml",
+            "http://qubership.org/schemas/product/qip/service/specification-group.schema.yaml", "samples/specification-group/*.yaml",
+            "http://qubership.org/schemas/product/qip/service/specification.schema.yaml", "samples/specification/*.yaml"
+    );
+
+    static class ThisTestArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(
+                ParameterDeclarations parameters,
+                ExtensionContext context
+        ) throws Exception {
+            PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            return testMap.entrySet().stream().flatMap(entry -> {
+                try {
+                    String schema = entry.getKey();
+                    Resource[] resources = resolver.getResources("classpath:" + entry.getValue());
+                    return Stream.of(resources).map(resource -> Arguments.of(schema, resource));
+                } catch (IOException exception) {
+                    logger.log(Level.SEVERE, exception.getMessage(), exception);
+                    return Stream.empty();
+                }
+            });
+        }
+    }
+
+    private static JsonSchemaFactory jsonSchemaFactory;
+    private static SchemaValidatorsConfig config;
+
+    @BeforeAll
+    public static void setUp() {
+        // This creates a schema factory that will use Draft-07 as the default if $schema is not specified
+        // in the schema data. If $schema is specified in the schema data, then that schema dialect will be used
+        // instead and this version is ignored.
+        jsonSchemaFactory = JsonSchemaFactory.getInstance(
+                SpecVersion.VersionFlag.V7,
+                builder -> builder.schemaMappers(schemaMappers -> schemaMappers.mappings(
+                    iri -> iri.startsWith("http://qubership.org/schemas/product/qip"),
+                    iri -> {
+                        String result = iri.replace("http://qubership.org/schemas/product/qip", "classpath:qip-model");
+                        logger.info(iri + " -> " + result);
+                        return result;
+                    }))
+        );
+        SchemaValidatorsConfig.Builder builder = SchemaValidatorsConfig.builder();
+
+        // By default, the JDK regular expression implementation which is not ECMA 262 compliant is used
+        // Note that setting this requires including optional dependencies
+        // builder.regularExpressionFactory(GraalJSRegularExpressionFactory.getInstance());
+        // builder.regularExpressionFactory(JoniRegularExpressionFactory.getInstance());
+        config = builder.build();
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(ThisTestArgumentsProvider.class)
+    public void testSchemaOnSample(String schemaId, Resource resource) throws IOException {
+        String source = resource.getContentAsString(Charset.defaultCharset());
+        JsonSchema schema = jsonSchemaFactory.getSchema(SchemaLocation.of(schemaId), config);
+        Set<ValidationMessage> assertions = schema.validate(source, InputFormat.YAML, executionContext -> {
+            executionContext.getExecutionConfig().setFormatAssertionsEnabled(true);
+        });
+        assertAll(schemaId + " -> " + resource.getURI().getPath(), assertions.stream().map(
+                validationMessage -> () -> fail(validationMessage.getMessage())));
+    }
+}
